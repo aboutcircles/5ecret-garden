8bc35811b38a9032a48b8071db777399e703456b
Vanshika Srivastava <vanshikasrivastava.business@gmail.com>
2025-02-25 01:09:50 +0530

    [WIP] handle Safe deployment

--- circles-app/src/lib/pages/SafeList.svelte
+++ circles-app/src/lib/pages/SafeList.svelte
@@ -1,115 +1,114 @@
 <script lang="ts">
-  import { shortenAddress } from '$lib/utils/shared';
-  import { SafeSdkBrowserContractRunner } from '@circles-sdk/adapter-safe';
-  import { onMount } from 'svelte';
-  import { ethers } from 'ethers6';
-  import ConnectCircles from '$lib/components/ConnectCircles.svelte';
-  import Avatar from '$lib/components/avatar/Avatar.svelte';
-  import { initializeWallet, wallet } from '$lib/stores/wallet';
-  import { avatar } from '$lib/stores/avatar';
-  import { circles } from '$lib/stores/circles';
-  import { Sdk } from '@circles-sdk/sdk';
-  import { goto } from '$app/navigation';
-  import { getCirclesConfig } from '$lib/utils/helpers';
-  import { fetchGroupsByOwner } from '$lib/utils/groups';
+    import { SafeSdkBrowserContractRunner } from '@circles-sdk/adapter-safe';
+    import { onMount } from 'svelte';
+    import { ethers } from 'ethers6';
+    import ConnectCircles from '$lib/components/ConnectCircles.svelte';
+    import Avatar from '$lib/components/avatar/Avatar.svelte';
+    import { initializeWallet, wallet } from '$lib/stores/wallet';
+    import { avatar } from '$lib/stores/avatar';
+    import { circles } from '$lib/stores/circles';
+    import { Sdk } from '@circles-sdk/sdk';
+    import { goto } from '$app/navigation';
+    import { getCirclesConfig } from '$lib/utils/helpers';
+    import { fetchGroupsByOwner } from '$lib/utils/groups';
 
-  let safes: string[] = [];
-  let groupsByAddress: Record<string, string[]>;
+    let safes: string[] = [];
+    let groupsByAddress: Record<string, string[]>;
 
-  const getSafesByOwnerApiEndpoint = (checksumOwnerAddress: string): string =>
-    `https://safe-transaction-gnosis-chain.safe.global/api/v1/owners/${checksumOwnerAddress}/safes/`;
+    const getSafesByOwnerApiEndpoint = (checksumOwnerAddress: string): string =>
+        `https://safe-transaction-gnosis-chain.safe.global/api/v1/owners/${checksumOwnerAddress}/safes/`;
 
-  async function querySafeTransactionService(
-    ownerAddress: string
-  ): Promise<string[]> {
-    const checksumAddress = ethers.getAddress(ownerAddress);
-    const requestUrl = getSafesByOwnerApiEndpoint(checksumAddress);
+    async function querySafeTransactionService(
+        ownerAddress: string
+    ): Promise<string[]> {
+        const checksumAddress = ethers.getAddress(ownerAddress);
+        const requestUrl = getSafesByOwnerApiEndpoint(checksumAddress);
 
-    const safesByOwnerResult = await fetch(requestUrl);
-    const safesByOwner = await safesByOwnerResult.json();
+        const safesByOwnerResult = await fetch(requestUrl);
+        const safesByOwner = await safesByOwnerResult.json();
 
-    return safesByOwner.safes ?? [];
-  }
-
-  onMount(loadSafesAndGroups);
-
-  async function loadSafesAndGroups() {
-    if (!$wallet) {
-      throw new Error('Wallet address is not available');
+        return safesByOwner.safes ?? [];
     }
 
-    const ownerAddress =
-      $wallet instanceof SafeSdkBrowserContractRunner
-        ? await $wallet.browserProvider.getSigner().then((s) => s.address)
-        : $wallet.address!;
+    onMount(loadSafesAndGroups);
 
-    safes = await querySafeTransactionService(ownerAddress);
+    async function loadSafesAndGroups() {
+        if (!$wallet) {
+            throw new Error('Wallet address is not available');
+        }
 
-    const groupFetchPromises = safes.map(async (safe) => {
-      const groups = await fetchGroupsByOwner(safe);
-      console.log(groups);
-      groupsByAddress = { ...groupsByAddress, [safe]: groups.flat() };
-    });
+        const ownerAddress =
+            $wallet instanceof SafeSdkBrowserContractRunner
+                ? await $wallet.browserProvider.getSigner().then((s) => s.address)
+                : $wallet.address!;
 
-    await Promise.all(groupFetchPromises);
-  }
+        safes = await querySafeTransactionService(ownerAddress);
 
-  //
-  // Connects the wallet and initializes the Circles SDK.
-  //
+        const groupFetchPromises = safes.map(async (safe) => {
+            const groups = await fetchGroupsByOwner(safe);
+            console.log(groups);
+            groupsByAddress = { ...groupsByAddress, [safe]: groups.flat() };
+        });
 
-  // bottomInfo={shortenAddress(item.toLowerCase()) +
-  //       (groupsByAddress[item].length > 0
-  //         ? ' - owner ' + groupsByAddress[item].length + ' groups'
-  //         : '')}
-
-  let manualSafeAddress: string = localStorage.getItem('manualSafeAddress') ?? '';
-
-  async function connectWallet(safeAddress: string) {
-    $wallet = await initializeWallet('safe', safeAddress);
-
-    const network = await $wallet?.provider?.getNetwork();
-    if (!network) {
-      throw new Error('Failed to get network');
-    }
-    var circlesConfig = await getCirclesConfig(network.chainId);
-
-    // Initialize the Circles SDK and set it as $circles to make it globally available.
-    $circles = new Sdk($wallet!, circlesConfig);
-
-    const avatarInfo = await $circles.data.getAvatarInfo(
-      $wallet.address!
-    );
-
-    // If the signer address is already a registered Circles wallet, go straight to the dashboard.
-    if (avatarInfo) {
-      $avatar = await $circles.getAvatar($wallet.address!);
-      await goto('/dashboard');
-    } else {
-      await goto('/register');
+        await Promise.all(groupFetchPromises);
     }
 
-    localStorage.setItem('manualSafeAddress', safeAddress);
-  }
+    //
+    // Connects the wallet and initializes the Circles SDK.
+    //
+
+    // bottomInfo={shortenAddress(item.toLowerCase()) +
+    //       (groupsByAddress[item].length > 0
+    //         ? ' - owner ' + groupsByAddress[item].length + ' groups'
+    //         : '')}
+
+    let manualSafeAddress: string = localStorage.getItem('manualSafeAddress') ?? '';
+
+    async function connectWallet(safeAddress: string) {
+        $wallet = await initializeWallet('safe', safeAddress);
+
+        const network = await $wallet?.provider?.getNetwork();
+        if (!network) {
+            throw new Error('Failed to get network');
+        }
+        var circlesConfig = await getCirclesConfig(network.chainId);
+
+        // Initialize the Circles SDK and set it as $circles to make it globally available.
+        $circles = new Sdk($wallet!, circlesConfig);
+
+        const avatarInfo = await $circles.data.getAvatarInfo(
+            $wallet.address!
+        );
+
+        // If the signer address is already a registered Circles wallet, go straight to the dashboard.
+        if (avatarInfo) {
+            $avatar = await $circles.getAvatar($wallet.address!);
+            await goto('/dashboard');
+        } else {
+            await goto('/register');
+        }
+
+        localStorage.setItem('manualSafeAddress', safeAddress);
+    }
 </script>
 
 
 <div class="w-full border rounded-lg flex justify-between items-center p-4 shadow-sm">
-  <input type="text" placeholder="Enter safe address .." bind:value={manualSafeAddress} class="w-full"/>
-  <img src="/chevron-right.svg" alt="Chevron Right" class="w-4 cursor-pointer" on:click={() => connectWallet(manualSafeAddress)}/>
+    <input type="text" placeholder="Enter safe address .." bind:value={manualSafeAddress} class="w-full"/>
+    <img src="/chevron-right.svg" alt="Chevron Right" class="w-4 cursor-pointer" on:click={() => connectWallet(manualSafeAddress)}/>
 </div>
 {#each safes ?? [] as item (item)}
-  <ConnectCircles address={item}>
-    <Avatar
-      address={item.toLowerCase()}
-      clickable={false}
-      view="horizontal"
+    <ConnectCircles address={item}>
+        <Avatar
+                address={item.toLowerCase()}
+                clickable={false}
+                view="horizontal"
 
-    />
-  </ConnectCircles>
+        />
+    </ConnectCircles>
 {/each}
 {#if (safes ?? []).length === 0}
-  <div class="text-center">
-    <p>No safes available.</p>
-  </div>
+    <div class="text-center">
+        <p>No safes available.</p>
+    </div>
 {/if}
