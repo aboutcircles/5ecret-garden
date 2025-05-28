ad1ab13201d39666e902b53a6ba86b47c4ec42db
Wagalidoom <lukas-8@live.fr>
2025-05-28 10:28:06 +0200

    feat: use connect account in safe selection

--- circles-app/src/routes/connect-wallet/connect-safe/+page.svelte
+++ circles-app/src/routes/connect-wallet/connect-safe/+page.svelte
@@ -1,43 +1,64 @@
 <script lang="ts">
   import ConnectSafe from '$lib/components/ConnectSafe.svelte';
-  import { GNOSIS_CHAIN_ID_DEC, initializeWallet, wallet } from '$lib/stores/wallet.svelte';
+  import { initializeWallet, wallet } from '$lib/stores/wallet.svelte';
   import { circles } from '$lib/stores/circles';
   import { onMount } from 'svelte';
   import { getCirclesConfig } from '$lib/utils/helpers.js';
   import { Sdk } from '@circles-sdk/sdk';
-  import { switchOrAddGnosisNetwork } from '$lib/utils/network';
   import { CirclesStorage } from '$lib/utils/storage';
   import { environment } from '$lib/stores/environment.svelte';
-
+  import {
+    connect,
+    getAccount,
+    getConnectors,
+    reconnect,
+    type GetAccountReturnType,
+  } from '@wagmi/core';
+  import { config } from '../../../config';
+  import type { Address } from '@circles-sdk/utils';
   let initialized: boolean | undefined = $state();
 
-  async function setup(callNo = 0) {
-    if (CirclesStorage.getInstance().walletType != 'safe') {
-      CirclesStorage.getInstance().data = {
-        avatar: undefined,
-        group: undefined,
-      };
-    }
-    $wallet = await initializeWallet('safe');
+  let account: GetAccountReturnType | undefined = $state();
 
-    const network = await ($wallet as any).provider?.getNetwork();
-    if (!network) {
-      throw new Error('Failed to get network');
+  async function setup(callNo = 0) {
+    // if (CirclesStorage.getInstance().walletType != 'safe') {
+    //   CirclesStorage.getInstance().data = {
+    //     avatar: undefined,
+    //     group: undefined,
+    //   };
+    // }
+    // $wallet = await initializeWallet('safe');
+
+    account = getAccount(config);
+    console.log(account);
+    if (!account.chainId) {
+      const connectorId = localStorage.getItem('connectorId');
+      const connector = getConnectors(config).find((c) => c.id == connectorId);
+      if (!connector) {
+        throw new Error('Connector not found');
+      }
+      const result = await connect(config, {
+        chainId: 100,
+        connector: connector,
+      });
+
+      account = getAccount(config);
     }
 
     if (callNo > 2) {
       return;
     }
 
-    // If we're on the wrong network, attempt to switch
-    if (![GNOSIS_CHAIN_ID_DEC].includes(network.chainId)) {
-      await switchOrAddGnosisNetwork();
-      await setup(callNo++);
-      return;
+    if (!account.chainId) {
+      throw new Error('Failed to get chainId');
     }
 
     // Initialize the Circles SDK and set it as $circles to make it globally available.
-    const circlesConfig = await getCirclesConfig(network.chainId, environment.ring);
+    const circlesConfig = await getCirclesConfig(
+      BigInt(account.chainId),
+      environment.ring
+    );
+    $wallet = await initializeWallet('safe');
     $circles = new Sdk($wallet!, circlesConfig);
 
     CirclesStorage.getInstance().data = {
@@ -50,11 +71,14 @@
     await setup();
     initialized = true;
   });
-
 </script>
 
-{#if !initialized}
+{#if !initialized || !account}
   Loading...
 {:else}
-  <ConnectSafe safeOwnerAddress={$wallet?.address} chainId={100n} walletType="safe" />
+  <ConnectSafe
+    safeOwnerAddress={account.address as Address}
+    chainId={100n}
+    walletType="safe"
+  />
 {/if}
