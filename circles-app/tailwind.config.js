23d0b3e60d13a395251faf7cba09bad508aef534
Wagalidoom <lukas-8@live.fr>
2025-06-02 16:58:44 +0200

    temp: remove daisyui config

--- circles-app/tailwind.config.js
+++ circles-app/tailwind.config.js
@@ -9,21 +9,4 @@ export default {
       },
     },
   },
-  plugins: [daisyui],
-  daisyui: {
-    themes: [
-      {
-        mytheme: {
-          primary: '#38318b',
-          secondary: '#4052d6',
-          accent: '#37cdbe',
-          neutral: '#3d4451',
-          'base-100': '#ffffff',
-        },
-      },
-      'dark',
-      'cupcake',
-    ],
-    darkTheme: 'light',
-  },
 };
