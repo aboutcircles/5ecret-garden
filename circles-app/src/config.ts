defba546b2ce93670af68dd441e88f408097a6d5
Wagalidoom <lukas-8@live.fr>
2025-07-02 15:43:04 +0200

    fix: remove injected connectors

--- circles-app/src/config.ts
+++ circles-app/src/config.ts
@@ -1,6 +1,6 @@
 import { http, createConfig } from '@wagmi/core';
 import { gnosis } from '@wagmi/core/chains';
-import { injected, walletConnect } from '@wagmi/connectors';
+import { walletConnect } from '@wagmi/connectors';
 
 export const config = createConfig({
     chains: [gnosis],
@@ -8,7 +8,6 @@ export const config = createConfig({
         [gnosis.id]: http(),
     },
     connectors: [
-        injected(),
         walletConnect({
             projectId: 'aeb9893f1c735720bcacaf6796bb541e',
         })
