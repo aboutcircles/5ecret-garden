67669df91a980fa3f471d56b1f2c9579aef8e4f3
Wagalidoom <lukas-8@live.fr>
2025-07-02 15:15:06 +0200

    feat: add wallet connect

--- circles-app/src/config.ts
+++ circles-app/src/config.ts
@@ -1,10 +1,16 @@
 import { http, createConfig } from '@wagmi/core';
 import { gnosis } from '@wagmi/core/chains';
-import { injected } from '@wagmi/connectors';
+import { injected, walletConnect } from '@wagmi/connectors';
 
 export const config = createConfig({
     chains: [gnosis],
     transports: {
         [gnosis.id]: http(),
     },
+    connectors: [
+        injected(),
+        walletConnect({
+            projectId: 'aeb9893f1c735720bcacaf6796bb541e',
+        })
+    ],
 })
