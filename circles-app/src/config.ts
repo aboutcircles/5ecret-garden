c4f9ef981be93692da5300b3cefb7375419198eb
Wagalidoom <lukas-8@live.fr>
2025-07-03 08:36:10 +0200

    fix: remove wallet connect

--- circles-app/src/config.ts
+++ circles-app/src/config.ts
@@ -1,15 +1,9 @@
 import { http, createConfig } from '@wagmi/core';
 import { gnosis } from '@wagmi/core/chains';
-import { walletConnect } from '@wagmi/connectors';
 
 export const config = createConfig({
     chains: [gnosis],
     transports: {
         [gnosis.id]: http(),
     },
-    connectors: [
-        walletConnect({
-            projectId: 'aeb9893f1c735720bcacaf6796bb541e',
-        })
-    ],
 })
