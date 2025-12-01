b709c50863b13f9d1f222c64d29ebc3a5243e43f
jaensen <jaensen>
2025-12-01 23:17:44 +0100

    feat: use netlify adapter

--- circles-app/svelte.config.js
+++ circles-app/svelte.config.js
@@ -1,4 +1,5 @@
 import adapter from '@sveltejs/adapter-node';
+import adapterNetlify from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from 'mdsvex';
 
@@ -16,7 +17,9 @@ const config = {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter(),
+    adapter: process.env.NETLIFY_BUILD === 'true'
+      ? adapterNetlify()
+      : adapter(),
   },
 };
 
