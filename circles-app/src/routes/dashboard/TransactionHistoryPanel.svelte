5a81481fd2c535816655ed8ada39d402d7f809aa
daniel <4954577+jaensen@users.noreply.github.com>
2025-12-11 23:32:57 +0100

    fix: un-controlled infinite scroll

--- circles-app/src/routes/dashboard/TransactionHistoryPanel.svelte
+++ circles-app/src/routes/dashboard/TransactionHistoryPanel.svelte
@@ -4,4 +4,4 @@
     import {transactionHistory} from '$lib/stores/transactionHistory';
 </script>
 
-<GenericList row={TransactionRow} store={transactionHistory} rowHeight={64} maxPlaceholderPages={2} expectedPageSize={25} />
+<GenericList row={TransactionRow} store={transactionHistory} rowHeight={64} maxPlaceholderPages={0} expectedPageSize={25} />
