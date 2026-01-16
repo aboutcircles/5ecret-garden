a4266a5a9b337213cbb11bb4094c90820e0121b5
Sandipan Kundu <sandipank.dev@gmail.com>
2026-01-16 15:47:37 +0530

    Netlify testing

--- circles-app/src/routes/+page.svelte
+++ circles-app/src/routes/+page.svelte
@@ -17,7 +17,7 @@
 <div class="page page-stack page--lg">
     <div class="min-h-screen flex flex-col items-center justify-center text-center">
         <img src="./illustration.png" alt="illustration" class="max-w-[340px]"/>
-        <h1 class="text-5xl font-medium">Money, Reimagined</h1>
+        <h1 class="text-5xl font-medium">Money, Reimagined Testing Version</h1>
         <p class="py-6 px-2">
             Mint personal Circles, build trust, and manage your community groups with the Circles Core app
         </p>
